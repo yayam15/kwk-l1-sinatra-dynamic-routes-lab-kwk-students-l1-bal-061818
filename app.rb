331b850d48b2b30cf87b615ce1741ce9521e6137
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 19:57:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,6 +24,15 @@ class App < Sinatra::Base
     word5=params[:word5].to_i
   end
 
-
-  
+  @operation = params[:operation]
+  @number1 = params[:number1]
+  @number2 = params[:number2]
+  if @operation == "add"
+    @number1 + @number2
+  elsif @operation == "subtract"
+    @number1 - @number2
+  elsif @operation == "multiply"
+    @number1 * @number2
+  else
+    
 end
