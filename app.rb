5ccebe1993a4c154deeaf70a5d6b7dd0d40ebd46
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 20:00:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,42 @@
-require_relative 'config/environment'
 
-class App < Sinatra::Base
-  # Write your code here!
+    require_relative 'config/environment'
 
-end
+    class App < Sinatra::Base
+      # Write your code here!
+      get '/reversename/:name' do
+        @name = params[:name]
+        "#{@name.reverse}"
+      end
+
+      get '/square/:number' do
+        @number = params[:number]
+        "#{@number.to_i*@number.to_i}"
+      end
+
+      get '/say/:number/:phrase' do
+        final_string = ""
+        (params[:number].to_i).times do
+          final_string += "#{params[:phrase]}\n"
+        end
+        final_string
+      end
+
+      get '/say/:word1/:word2/:word3/:word4/:word5' do
+        "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+      end
+
+      get '/:operation/:number1/:number2' do
+        @operation = params[:operation]
+        @number1 = params[:number1].to_i
+        @number2 = params[:number2].to_i
+        if @operation == "add"
+          "#{@number1+@number2}"
+        elsif @operation == "subtract"
+          "#{@number1-@number2}"
+        elsif @operation == "multiply"
+          "#{@number1*@number2}"
+        elsif @operation == "divide"
+          "#{@number1/@number2}"
+        end
+      end
+    end
