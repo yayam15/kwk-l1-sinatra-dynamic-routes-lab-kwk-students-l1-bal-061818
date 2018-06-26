3a37172127d7976877f13537559d93d3b0ee7776
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 19:36:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
 get '/say/:word1/:word2/:word3/:word4/:word5' do
   word1=params[:word1].to_i
   word2=params[:word2].to_i
-
-
+  word3=params[:word3].to_i
+  word4=params[:word4].to_i
+  word5=params[:word5].to_i
 end
