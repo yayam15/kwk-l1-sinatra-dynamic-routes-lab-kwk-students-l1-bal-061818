b20e0a1f48afab94896e4c8561d09bf2400eb847
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 19:40:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
     num = params[:number]
     (num.to_i ** 2).to_s
   end
+  get '/say/:number/:phrase'
   
 get '/say/:word1/:word2/:word3/:word4/:word5' do
   word1=params[:word1].to_i
