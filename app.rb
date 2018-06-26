98d2f92adbea9473bb1e330f5d4939823ab3bcea
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 19:59:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,38 +1,43 @@
-require_relative 'config/environment'
 
-class App < Sinatra::Base
+    require_relative 'config/environment'
 
-  get '/reversename/:name'do
-    params[:name].reverse
-  end
+    class App < Sinatra::Base
+      # Write your code here!
+      get '/reversename/:name' do
+        @name = params[:name]
+        "#{@name.reverse}"
+      end
 
-  get '/square/:number' do
-    num = params[:number]
-    (num.to_i ** 2).to_s
-  end
-  get '/say/:number/:phrase' do
-    @number=params[:num].to_i
-    @phrase=params[:phra]
-    @number*@phrase
-  end
+      get '/square/:number' do
+        @number = params[:number]
+        "#{@number.to_i*@number.to_i}"
+      end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    word1=params[:word1].to_i
-    word2=params[:word2].to_i
-    word3=params[:word3].to_i
-    word4=params[:word4].to_i
-    word5=params[:word5].to_i
-  end
+      get '/say/:number/:phrase' do
+        final_string = ""
+        (params[:number].to_i).times do
+          final_string += "#{params[:phrase]}\n"
+        end
+        final_string
+      end
 
-  @operation = params[:operation]
-  @number1 = params[:number1]
-  @number2 = params[:number2]
-  if @operation == "add"
-    @number1 + @number2
-  elsif @operation == "subtract"
-    @number1 - @number2
-  elsif @operation == "multiply"
-    @number1 * @number2
-  else
-    
+      get '/say/:word1/:word2/:word3/:word4/:word5' do
+        "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+      end
+
+      get '/:operation/:number1/:number2' do
+        @operation = params[:operation]
+        @number1 = params[:number1].to_i
+        @number2 = params[:number2].to_i
+        if @operation == "add"
+          "#{@number1+@number2}"
+        elsif @operation == "subtract"
+          "#{@number1-@number2}"
+        elsif @operation == "multiply"
+          "#{@number1*@number2}"
+        elsif @operation == "divide"
+          "#{@number1/@number2}"
+        end
+      end
+    end
 end
