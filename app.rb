b368fccb211d5f0159e7257d1ae8e756c3d3266c
yayam15 <yazmyn.a.aguilar04@gmail.com>
2018-06-26 19:44:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ class App < Sinatra::Base
     (num.to_i ** 2).to_s
   end
   get '/say/:number/:phrase' do
-    
+    @number=params[:num].to_i
+    @phrase=params[:phra]
+    @number*@phrase
   end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
